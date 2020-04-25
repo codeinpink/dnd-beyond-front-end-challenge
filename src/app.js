ae1c621a119ccbc4bd84638e6d35326e2a1f5153
Shannon Babincsak <shannon.babincsak@gmail.com>
2020-04-25 18:17:38 -0500

    Initial commit 🤠

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { Component, createElement as e } from 'react';
+
+class App extends Component {
+    render() {
+        return e('h1', null, 'Hello World');
+    }
+}
+
+export default App;
