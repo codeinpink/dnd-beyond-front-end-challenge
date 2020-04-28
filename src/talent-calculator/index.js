9ba649d88030e5c01dd4553fc253a4c816720cf4
Shannon Babincsak <shannon.babincsak@gmail.com>
2020-04-27 22:44:22 -0500

    Make the talent calculator actually work 🎉

--- src/talent-calculator/index.js
+++ src/talent-calculator/index.js
@@ -2,27 +2,45 @@ import React from 'react';
 import Talent from './talent';
 import style from './style.scss';
 
+const MAX_POINTS = 6;
+
 class TalentCalculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            talents: {
+                'Talent Path 1': [false, false, false, false],
+                'Talent Path 2': [false, false, false, false],
+            },
+            pointsSpent: 0
+        };
+    }
+
     render() {
         return(
             <div className="talent-calculator">
                 <h2>TitanStar Legends - Rune Mastery Loadout Talent Calculator 9000</h2>
+                <div className="talents">
+                    {this.renderTalentPath('Talent Path 1', ['Stack', 'Silverware', 'Cake', 'Crown'])}
 
-                {this.renderTalentPath('Talent Path 1', ['Stack', 'Silverware', 'Cake', 'Crown'])}
-
-                {this.renderTalentPath('Talent Path 2', ['Boat', 'Scuba', 'Lightning', 'Skull'])}
+                    {this.renderTalentPath('Talent Path 2', ['Boat', 'Scuba', 'Lightning', 'Skull'])}
+                </div>
+                <h3>{this.state.pointsSpent}/{MAX_POINTS} Points Spent</h3>
             </div>
         );
     }
 
-    renderTalentPath(title, talents) {
-        const talentList = talents.map(talent => {
+    renderTalentPath(pathTitle, talents) {
+        const talentList = talents.map((talent, talentIndex) => {
             return(
                 <li key={talent}>
                     <Talent
                         name={talent}
-                        disabled={true}
-                        onClick={() => this.handleClick()}
+                        disabled={this.isTalentDisabled(pathTitle, talentIndex)}
+                        selected={this.isTalentSelected(pathTitle, talentIndex)}
+                        onLeftClick={(e) => this.handleLeftClick(e, pathTitle, talentIndex)}
+                        onRightClick={(e) => this.handleRightClick(e, pathTitle, talentIndex)}
+                        onKeyDown={(e) => this.handleKeyDown(e, pathTitle, talentIndex)}
                     ></Talent>
                 </li>
             );
@@ -30,7 +48,7 @@ class TalentCalculator extends React.Component {
 
         return(
             <div className="talent-path">
-                <h3>{title}</h3>
+                <h3>{pathTitle}</h3>
                 <ol>
                     {talentList}
                 </ol>
@@ -38,8 +56,78 @@ class TalentCalculator extends React.Component {
         );
     }
 
-    handleClick() {
-        console.log('handleClick');
+    isTalentDisabled(pathTitle, talentIndex) {
+        if (this.state.pointsSpent === MAX_POINTS && !this.isTalentSelected(pathTitle, talentIndex)) {
+            return true;
+        }
+
+        if (talentIndex === 0) {
+            return false;
+        }
+
+        const path = this.state.talents[pathTitle];
+        const previousSelected = path[talentIndex - 1] === true;
+        return !previousSelected;
+    }
+
+    isTalentSelected(pathTitle, talentIndex) {
+        return this.state.talents[pathTitle][talentIndex] === true;
+    }
+
+    handleLeftClick(event, pathTitle, talentIndex) {
+        const points = this.state.pointsSpent;
+        const path = this.state.talents[pathTitle];
+
+        if (!path[talentIndex]) {
+            this.setState({pointsSpent: points + 1});
+            this.updateTalent(pathTitle, talentIndex, true);
+        }
+    }
+
+    handleRightClick(event, pathTitle, talentIndex) {
+        event.nativeEvent.preventDefault(); // prevent context menu from popping up
+        const points = this.state.pointsSpent;
+        const path = this.state.talents[pathTitle];
+
+        if (path[talentIndex]) {
+            this.setState({pointsSpent: points - 1});
+            this.updateTalent(pathTitle, talentIndex, false);
+        }
+    }
+
+    handleKeyDown(event, pathTitle, talentIndex) {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.nativeEvent.preventDefault();
+
+            const points = this.state.pointsSpent;
+            const path = this.state.talents[pathTitle];
+
+            const talentAlreadySelected = path[talentIndex] === true;
+            const newPoints = talentAlreadySelected ? points - 1 : points + 1;
+            const newValue = talentAlreadySelected ? false : true;
+
+            this.setState({pointsSpent: newPoints});
+            this.updateTalent(pathTitle, talentIndex, newValue);
+        }
+    }
+
+    updateTalent(pathTitle, talentIndex, newValue) {
+        const talentsCopy = JSON.parse(JSON.stringify(this.state.talents));
+        talentsCopy[pathTitle][talentIndex] = newValue;
+
+        // Removing a talent should clear the rest of the talents that come after
+        if (!newValue) {
+            for (let i = talentIndex; i < talentsCopy[pathTitle].length; i++) {
+                if (talentsCopy[pathTitle][i]) {
+                    talentsCopy[pathTitle][i] = false;
+                    this.setState((state) => {
+                        return {pointsSpent: state.pointsSpent - 1};
+                    });
+                }
+            }
+        }
+
+        this.setState({talents: talentsCopy});
     }
 }
 
--- src/talent-calculator/talent/index.js
+++ src/talent-calculator/talent/index.js
@@ -3,11 +3,13 @@ import style from './style.scss';
 
 class Talent extends React.Component {
     render() {
-        const classes = `talent talent-${this.props.name}`;
+        const classes = `talent talent-${this.props.name} ${this.props.selected ? 'selected' : ''}`;
         return(
             <button
                 disabled={this.props.disabled}
-                onClick={() => this.props.onClick()}
+                onClick={(e) => this.props.onLeftClick(e)}
+                onContextMenu={(e) => this.props.onRightClick(e)}
+                onKeyDown={(e) => this.props.onKeyDown(e)}
                 className={classes}
                 aria-label={this.props.name}
             ></button>
