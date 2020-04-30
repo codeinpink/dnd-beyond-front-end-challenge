eaba5c70608fa3aa2ecfa3404baf47c6f8bd1fdf
Shannon Babincsak <shannon.babincsak@gmail.com>
2020-04-29 20:55:56 -0500

    Update main menu

--- src/header/main-menu/index.js
+++ src/header/main-menu/index.js
@@ -4,13 +4,20 @@ import style from './style.scss';
 class MainMenu extends React.Component {
     render() {
         return(
-            <nav className="main-menu">
+            <nav aria-label="Main Navigation" className="main-menu">
                 <ul>
-                    <li><a href="#">Snowflakes</a></li>
-                    <li><a href="#">Magnets</a></li>
                     <li>
+                        <i className="snowflakes-icon" aria-hidden="true"></i>
+                        <a href="#">Snowflakes</a>
+                    </li>
+                    <li>
+                        <i className="magnets-icon" aria-hidden="true"></i>
+                        <a href="#">Magnets</a>
+                    </li>
+                    <li className="nested-menu">
+                        <i className="liquids-icon" aria-hidden="true"></i>
                         <a href="#">Liquids</a>
-                        <ul>
+                        <ul className="menu">
                             <li><a href="#">Flammable</a></li>
                             <li><a href="#">Combustable</a></li>
                             <li><a href="#">Flambustable</a></li>
